32fcaa16c4c3abf13f4474e0f5e07e51ef51d2e0
Dalay20 <137733175+Dalay20@users.noreply.github.com>
2025-07-28 19:43:56 +0000

    Guia_21

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import firebase_admin
+from firebase_admin import credentials
+
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CREDENTIALS_PATH = credentials.Certificate("secrets/landing-key.json")
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(FIREBASE_CREDENTIALS_PATH, {
+   'databaseURL': 'https://landingpage-d5016-default-rtdb.firebaseio.com/'
+})
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +49,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "homepage",
+    "landing_api",
     "demo_rest_api",
+    "firebase_admin",
 ]
 
 MIDDLEWARE = [
